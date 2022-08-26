93ca3d03b9cb974d6a35757ee5fe9c94dc4dc3cf
Rchard-Lee <66902904+Rchard-Lee@users.noreply.github.com>
2022-08-26 17:03:44 +0800

    8-26：1. 调整reactive的代码结构2. 实现effect的基本框架

---
+++ packages/reactivity/src/baseHandler.ts
@@ -0,0 +1,67 @@
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+}
+
+import { activeEffect } from "./effect";
+export const mutableHandlers = {
+  // target:目标对象; key:被获取的属性名; receiver:Proxy 或者继承 Proxy 的对象
+  get(target, key, receiver) {
+    // 去代理的目标对象上取值，走get，可以监控用户取值
+
+    // 当前对象已经被代理过了
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return true;
+    }
+
+    // !! 不能按下面这种方式去返回值（原因见最后注意事项），而是要用Reflect
+    // return target[key]
+
+    return Reflect.get(target, key, receiver);
+  },
+  set(target, key, value, receiver) {
+    // 去代理的目标对象上设置值，走set，可以监控用户设置值
+
+    // !! 不能按下面这种方式去设置值（原因同get），而是要用Reflect
+    // target[key] = value;
+    // return true;
+
+    return Reflect.set(target, key, value, receiver);
+  },
+};
+
+// -------------------------------------------注意事项-----------------------------------------------------
+// // 1. Proxy的get/set去原对象取值和设置值，都要用Reflect，原因如下
+
+// // 当有一个对象，对象中有get、set这种定义属性方式
+// let target = {
+//   name: "lcd",
+//   get alias() {
+//     return this.name;
+//   },
+// };
+// // 思考：当页面中使用了alias对应的值，稍后name值变化了，要重新渲染alias吗？当然要
+
+// const proxyTest = new Proxy(target, {
+//   get(target, key, receiver) {
+//     // 如果用 return target[key]，那么当调用proxyTest.alias的时候，只会触发一次get
+//     // 这是因为this.name中的this指的时target，而target是一个普通对象，不会走到proxyTest中的get中来，相当于没有监控到name属性
+//     // 但是按理说get alias的时候，也要触发get name，总共要触发两次proxyTest的get
+//     // return target[key]
+
+//     // 所以要用Reflect.get,这时相当于用receiver代替了上面target对象上get alias函数中的this
+//     // 所以此时去获取name的值的时候，也会走proxy的get，也会触发get name
+//     console.log(key);
+//     return Reflect.get(target, key, receiver);
+//   },
+//   set(target, key, value, receiver) {
+//     // 去代理的目标对象上设置值，走set
+
+//     // !! 不能按下面这种方式去设置值（原因同get），而是要用Reflect
+//     // target[key] = value;
+//     // return true;
+
+//     return Reflect.set(target, key, value, receiver);
+//   },
+// });
+
+// proxyTest.alias;
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,1 +1,33 @@
-export function effect() {}
+export let activeEffect = undefined;
+class ReactiveEffect {
+  public active = true; // 控制effect的激活状态，默认是激活状态
+
+  constructor(public fn) {}
+
+  // run就是执行effect
+  run() {
+    // 如果是非激活状态，只需要执行函数，不需要进行依赖收集
+    if (!this.active) {
+      this.fn();
+    }
+
+    // 下面进行依赖收集
+    // 核心是：将当前的 effect 和 稍后渲染的属性 关联在一起
+    try {
+      activeEffect = this; // 稍后调用fn，fn内部取值操作的时候，就可以获取到这个全局的activeEffect
+      return this.fn();
+    } finally {
+      // 清除当前的effect
+      activeEffect = undefined;
+    }
+  }
+}
+
+// effect可以嵌套着写： effect(() => { state.name; effect(() => {state.age})})
+export function effect(fn) {
+  // fn可以根据状态变化重新执行
+
+  const _effect = new ReactiveEffect(fn); // 创建响应式的effect
+  // 默认先执行一次
+  _effect.run();
+}
